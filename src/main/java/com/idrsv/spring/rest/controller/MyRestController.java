df83b4135539811e32a32b3a2e72bd2b637aeb35
idrsv <idrisovds@gmail.com>
2022-10-26 20:24:14 +0300

    Edit MyRestController, add package exception_handling

--- src/main/java/com/idrsv/spring/rest/controller/MyRestController.java
+++ src/main/java/com/idrsv/spring/rest/controller/MyRestController.java
@@ -1,15 +1,17 @@
 package com.idrsv.spring.rest.controller;
 
 import com.idrsv.spring.rest.entity.Employee;
+import com.idrsv.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.idrsv.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.idrsv.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController          //контроллер, управляющий рест запросами и ответами
+@RestController          //контроллер, управляющий Rest запросами и ответами
 @RequestMapping("/api")
 public class MyRestController {
 
@@ -22,6 +24,19 @@ public class MyRestController {
 
         return allEmployees;
     }
+
+    @GetMapping("/employees/{id}")  //получаем id из самого url адреса
+    public Employee showEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no  employee with ID = " + id + " in DataBase");
+        }
+
+        return employee;
+    }
+
+
 }
 
 
---
+++ src/main/java/com/idrsv/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,16 @@
+package com.idrsv.spring.rest.exception_handling;
+
+public class EmployeeIncorrectData {
+    private String info;
+
+    public EmployeeIncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
